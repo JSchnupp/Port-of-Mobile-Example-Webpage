4f7f017627b4d1b9418e5c9790d4fbce2fe77845
JSchnupp <jor.schnupp@gmail.com>
2025-04-30 13:24:52 -0500

    refactor: update import paths for WarehouseForm component to use absolute paths

--- app/components/forms/WarehouseForm.tsx
+++ app/components/forms/WarehouseForm.tsx
@@ -1,15 +1,15 @@
 import * as React from "react"
-import { Button } from "../../components/ui/button"
+import { Button } from "@/components/ui/button"
 import {
   Card,
-  CardContent, // this is the right one that jordyn didnt mean to push
+  CardContent,
   CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
-} from "../../components/ui/card"
-import { Input } from "../../components/ui/input"
-import { Label } from "../../components/ui/label"
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 import { useState } from "react"
 
 interface WarehouseFormProps {
